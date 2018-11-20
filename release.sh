90730341689615a56f30ccf7fe62e1995b8a263e
Karol Fabjańczuk <karol.fabjanczuk@gmail.com>
2018-11-20 11:16:05 +0100

    first commit

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Get new tags from remote
+git fetch --tags
+# Get latest tag name
+latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+# remove 'v' from latestTag
+latestTag=${latestTag#"v"}
+
+echo "Enter release version (latest is $latestTag): "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Releasing $VERSION ..."
+
+    # commit
+    yarn version --new-version $VERSION --message "[release] $VERSION"
+
+    # publish
+    git push origin refs/tags/v$VERSION
+    git push
+fi
